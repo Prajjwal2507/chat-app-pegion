17cc3159092619be4684cb0743294be73d15122e
Prajjwal2507 <prajjwalrajput2507@gmail.com>
2026-03-17 00:06:40 +0530

    fix frontend path for render

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,20 +10,20 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 // make ready for deployment
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-
-  app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
-
 }
 
 app.listen(PORT, () => {
