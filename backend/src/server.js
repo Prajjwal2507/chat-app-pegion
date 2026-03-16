43329995da4066d49343680ac63157c67142a387
Prajjwal2507 <prajjwalrajput2507@gmail.com>
2026-03-16 23:46:34 +0530

    change in dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use("/api/auth" , authRoutes);
 app.use("/api/message" , messageRoutes);
 
 //make ready for deployment
-if(process.env.NODE_ENV === "production")
+if(process.env.NODE_ENV === "production") 
 {
    app.use(express.static(path.join(__dirname , "../frontend/dist")));
 
