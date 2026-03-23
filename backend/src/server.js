8070ba8b2c28b69c5265c924c190a489479d2f67
Prajjwal2507 <prajjwalrajput2507@gmail.com>
2026-03-23 18:46:58 +0530

    welcome email added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,38 +1,33 @@
 import express from "express";
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
-dotenv.config();
-
+//dotenv.config();
 
 const app = express();
 const __dirname = path.resolve();
 
+const PORT = ENV.PORT || 3000;
 
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());    // for parsing the json data that is comming from body
-app.use("/api/auth" , authRoutes);
-app.use("/api/message" , messageRoutes);
+app.use(express.json()); // for parsing the json data that is comming from body
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 //make ready for deployment
-if(process.env.NODE_ENV === "production") 
-{
-   app.use(express.static(path.join(__dirname , "../frontend/dist")));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-   });
+  });
 }
-
 
 app.listen(PORT, () => {
   console.log(`server running on port: ${PORT}`);
   connectDB();
-  
 });
